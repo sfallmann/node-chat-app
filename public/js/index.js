edfdce499416f0294fd44fa8d8b595ffe718329e
Sean Fallmann <jorundr@hotmail.com>
2017-03-11 08:19:09 -0500

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -9,5 +9,23 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('Got message:', message);
+  var newMsg = $('<li />', {
+    text: message.from + ': ' + message.text
+  });
+
+  $('#message-list').append(newMsg);
+
+  console.log('newMessage:', message);
+});
+
+$('#message-form').on('submit', function(event) {
+  event.preventDefault();
+
+  var text = $('[name=message]').val();
+  socket.emit('createMessage', {
+    from: 'form user',
+    text: text
+  }, function receipt(data){
+    console.log('Server receipt: ', data);
+  });
 });
--- server/server.js
+++ server/server.js
@@ -17,15 +17,19 @@ app.use(express.static(publicPath));
 io.on('connection', function connection(socket) {
   console.log('New user connected.');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat!');
+  socket.emit('newMessage',
+  generateMessage('Admin', 'Welcome to the chat!'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined'));
+  socket.broadcast.emit('newMessage',
+  generateMessage('Admin', 'New user has joined'));
 
-
-  socket.on('createMessage', function createEmail(message) {
+  socket.on('createMessage', function createEmail(message, callback) {
     console.log('Create message', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
 
+    if (typeof callback === 'function'){
+      callback('This is from the server');
+    }
   });
 
   socket.on('disconnect', function disconnect() {
